e63f3b95b3c2e6138eef4fbee259cdb908075e49
OzMale <mjkeely@gmail.com>
2018-04-19 11:18:40 +0100

    environements dir ignored

---
+++ src/environments/facebook.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3bTMiFRD3fF3SHcSgPuKyY5qA5kXh7eU",
+  authDomain: "movies-88185.firebaseapp.com",
+  databaseURL: "https://movies-88185.firebaseio.com",
+  storageBucket: ""
+};
