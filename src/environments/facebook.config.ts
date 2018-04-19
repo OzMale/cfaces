aeeb7bfde7aa916a838809f496b7651047da9ca5
Michael Keely <mjkeely@gmail.com>
2018-04-19 11:22:13 +0100

    Delete facebook.config.ts

--- src/environments/facebook.config.ts
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD3bTMiFRD3fF3SHcSgPuKyY5qA5kXh7eU",
-  authDomain: "movies-88185.firebaseapp.com",
-  databaseURL: "https://movies-88185.firebaseio.com",
-  storageBucket: ""
-};
